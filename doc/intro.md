a13186a4f129c9589eb34a491d1d24ffa6d5419b
leetwin <vili1216@netcracker.com>
2017-08-10 13:44:26 +0300

    initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to trivial-partial
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/trivial_partial/core_test.clj
@@ -0,0 +1,7 @@
+(ns trivial-partial.core-test
+  (:require [clojure.test :refer :all]
+            [trivial-partial.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
