b31ca49878f6395256962a81028a5624b7a5336e
leetwin <vili1216@netcracker.com>
2017-08-18 11:29:52 +0300

    [master] added macro expansion tests

--- test/trivial_partial/core_test.clj
+++ test/trivial_partial/core_test.clj
@@ -1,4 +1,53 @@
 (ns trivial-partial.core-test
   (:require [clojure.test :refer :all]
+            [clojure.string :as s]
+            [clojure.walk :refer [prewalk-replace macroexpand-all]]
+            [trivial-partial.utils :refer [deep-replace-with-seq]]
             [trivial-partial.core :refer :all]))
 
+(defn- find-generated-args [data]
+  (->> data
+     (tree-seq coll? seq)
+     (filter #(and (symbol? %)
+                 (s/starts-with? (name %) "ARG_")))
+     distinct))
+
+(defn- replace-generated-args [data]
+  (prewalk-replace (zipmap (find-generated-args data)
+                           (map (comp keyword (partial str "ARG_"))
+                                (range)))
+                   data))
+
+(defmacro generated-forms-equal? [expected actual]
+  (let [actual-rep (-> actual macroexpand-all replace-generated-args)]
+    `(= '~expected '~actual-rep)))
+
+(defn- subclass? [child-class parent-class]
+  ((conj (supers child-class) child-class) parent-class))
+
+(defmacro expansion-throws? [error-class form]
+  (try (macroexpand-all form)
+       nil
+       (catch Throwable err
+         `(subclass? ~(class err) ~error-class))))
+
+(deftest make-partial-test
+  (testing "simple make-partial"
+    (is (generated-forms-equal? (fn* ([:ARG_0 :ARG_1]
+                                      (+ 1 2 :ARG_0 4 :ARG_1 6)))
+                                (make-partial + [1 2 __ 4 __ 6]))))
+  (testing "make-partial with custom placeholder"
+    (is (generated-forms-equal? (fn* ([:ARG_0 :ARG_1 :ARG_2]
+                                      (str 1 :ARG_0 [3 :ARG_1 5] (6 :ARG_2))))
+                                (make-partial str [1 __x [3 __x 5] (6 __x)]
+                                              :placeholder __x))))
+  (testing "make-partial with literal sets and maps without placeholders"
+    (is (generated-forms-equal? (fn* ([:ARG_0]
+                                      (str 1 2 :ARG_0 {:a 10 :b #{1 2 3}})))
+                                (make-partial str [1 2 __ {:a 10 :b #{1 2 3}}]))))
+  (testing "make-partial with literal sets and maps with placeholders should throw error"
+    (is (expansion-throws? AssertionError
+                           (make-partial str [1 2 __ {:a 10 :b #{1 __ 3}}])))
+    
+    (is (expansion-throws? AssertionError
+                           (make-partial str [1 2 __ {:a 10 :b #{1 {:x __} 3}}])))))
--- test/trivial_partial/utils_test.clj
+++ test/trivial_partial/utils_test.clj
@@ -1,6 +1,6 @@
 (ns trivial-partial.utils-test
   (:require [trivial-partial.utils :as sut]
-            [clojure.test :as t :refer [deftest is]]))
+            [clojure.test :as t :refer [deftest is testing]]))
 
 (deftest deep-count-items-test
   (is (== 5
@@ -9,54 +9,66 @@
                                      #{:a :b :x}
                                      :x
                                      (:p {10 :x} :q)])))
-  (is (== 1
-          (sut/deep-count-items :x '[{:a 10 :b 20}
-                                     {:key1 :x}
-                                     (1 {:key :x} :x)] :item-key :key)))
-  (is (== 3
-          (sut/deep-count-items 100 '[1 99 3 [102 5 {6 101} 10]]
-                                :item-key #(when (number? %) (+ 10 %))
-                                :compare-fn #(and (number? %2) (> %2 %1))))))
+  (testing "deep-count-items with :item-key"
+    (is (== 1
+            (sut/deep-count-items :x '[{:a 10 :b 20}
+                                       {:key1 :x}
+                                       (1 {:key :x} :x)] :item-key :key))))
+  (testing "deep-count-items with :item-key and :compare-fn"
+    (is (== 3
+            (sut/deep-count-items 100 '[1 99 3 [102 5 {6 101} 10]]
+                                  :item-key #(when (number? %) (+ 10 %))
+                                  :compare-fn #(and (number? %2) (> %2 %1)))))))
 
 (deftest deep-find-test
-  (is (= :x
-         (sut/deep-find :x {:a 10 :b [20 #{1 2 :x}]})))
-  (is (= nil
-         (sut/deep-find :x {:a 10 :b [20 #{1 2}]})))
-  (is (= {:a 10}
-         (sut/deep-find 10 {:x [1 2 {:a 20} 3 #{4 5 {:a 10}}]}
-                        :item-key :a)))
-  (is (= {:a 100}
-         (sut/deep-find 50 [1 2 3 [{:a 50} {:a 100}]]
-                        :item-key :a
-                        :compare-fn #(and (number? %2) (> %2 %1))))))
+  (testing "deep-find finds item"
+    (is (= :x
+           (sut/deep-find :x {:a 10 :b [20 #{1 2 :x}]}))))
+  (testing "deep-find returns nil in case of no item in coll"
+    (is (= nil
+           (sut/deep-find :x {:a 10 :b [20 #{1 2}]}))))
+  (testing "deep-find with :item-key"
+    (is (= {:a 10}
+           (sut/deep-find 10 {:x [1 2 {:a 20} 3 #{4 5 {:a 10}}]}
+                          :item-key :a))))
+  (testing "deep-count with :item-key and :compare-fn"
+    (is (= {:a 100}
+           (sut/deep-find 50 [1 2 3 [{:a 50} {:a 100}]]
+                          :item-key :a
+                          :compare-fn #(and (number? %2) (> %2 %1)))))))
 
 (deftest deep-replace-with-seq-test
-  (is (= [1 2 [3 [:a :b] 4 '(10 [30 40] 20) {:x 100} #{:a :s :d}]]
-         (sut/deep-replace-with-seq
-          :rpl
-          [2 :a 30 20 :x :s]
-          [1 :rpl [3 [:rpl :b] 4 '(10 [:rpl 40] :rpl) {:rpl 100} #{:a :rpl :d}]])))
+  (testing "deep-replace with enough with enough replacements"
+    (is (= [1 2 [3 [:a :b] 4 '(10 [30 40] 20) {:x 100} #{:a :s :d}]]
+           (sut/deep-replace-with-seq
+            :rpl
+            [2 :a 30 20 :x :s]
+            [1 :rpl [3 [:rpl :b] 4 '(10 [:rpl 40] :rpl) {:rpl 100} #{:a :rpl :d}]]))))
 
-  (is (= [1 2 [3 [:a :b] 4 '(10 [30 40] 20)]]
-         (sut/deep-replace-with-seq
-          :rpl
-          [2 :a 30 20 :x :s :a :b :c :d :e :f :g]
-          [1 :rpl [3 [:rpl :b] 4 '(10 [:rpl 40] :rpl)]])))
+  (testing "deep-replace with more replacements then replaced values"
+    (is (= [1 2 [3 [:a :b] 4 '(10 [30 40] 20)]]
+           (sut/deep-replace-with-seq
+            :rpl
+            [2 :a 30 20 :x :s :a :b :c :d :e :f :g]
+            [1 :rpl [3 [:rpl :b] 4 '(10 [:rpl 40] :rpl)]]))))
 
-  (is (thrown? IllegalArgumentException
-               (sut/deep-replace-with-seq
-                :rpl
-                [2]
-                [1 :rpl [3 [:rpl :b] 4 '(10 [:rpl 40] :rpl)]])))
+  (testing "deep-replace with not enough replacements throws Illegalargumentexception"
+    (is (thrown? IllegalArgumentException
+                 (sut/deep-replace-with-seq
+                  :rpl
+                  [2]
+                  [1 :rpl [3 [:rpl :b] 4 '(10 [:rpl 40] :rpl)]]))))
 
-  (is (= [1 [2 3 4] [5 [6 7] 8]]
-         (sut/deep-replace-with-seq 0 [3 6]
-                                    [1 [2 100 4] [5 [100 7] 8]]
-                                    :item-key #(and (number? %) (- % 100)))))
+  (testing "deep-replace with :item-key"
+    (is (= [1 [2 3 4] [5 [6 7] 8]]
+           (sut/deep-replace-with-seq 0 [3 6]
+                                      [1 [2 100 4] [5 [100 7] 8]]
+                                      :item-key #(and (number? %) (- % 100))))))
 
-  (is (= [1 [2 3 4] [5 [6 100] 8]]
-         (sut/deep-replace-with-seq 50 [3 6]
-                                    [1 [2 101 4] [5 [102 100] 8]]
-                                    :item-key #(and (number? %) (/ % 2))
-                                    :compare-fn #(and (number? %2) (> %2 %1))))))
+  (testing "deep-replace with :item-key and :compare-fn"
+    (is (= [1 [2 3 4] [5 [6 100] 8]]
+           (sut/deep-replace-with-seq 50 [3 6]
+                                      [1 [2 101 4] [5 [102 100] 8]]
+                                      :item-key #(and (number? %) (/ % 2))
+                                      :compare-fn #(and (number? %2) (> %2 %1)))))))
+
